30a1bd4f1d579958735b643d304fc9cd8cf6f5d9
nandakumar2005 <nandakumar.parthasarathi@fwsa.freshworks.com>
2023-02-27 16:22:48 +0530

    updated

---
+++ assets/anime js/index anime.js
@@ -0,0 +1,24 @@
+// let tl = anime.timeline({
+//     loop:true,
+//     direction:'alternative',
+//     delay:300,
+//     endDelay: 300,
+//     easing: 'easeInoutExpo',
+//     duration: 2400
+// });
+
+// tl.add({
+//     targets: '.parallax-text.front',
+//     translateY: '-5vw'
+// }, 0)
+// .add({
+//     targets: '.parallax-text.back',
+//     translateY: '5vw'
+// }, 0)
+// .add({
+//     targets: '.img',
+//     translateY: '-2.5vw',
+//     boxShadow:
+//     '0 10px 20px rgba(0,0,0, 0.82)'
+// }, 0);
+
