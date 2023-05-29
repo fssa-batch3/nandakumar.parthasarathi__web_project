468e175f17bba2905bd665d7e2e9e29ce04ff1b5
Nandakumar Parthasarathi <nparthasarathi@fssa.freshworks.com>
2023-05-29 13:34:16 +0530

    updated

---
+++ pages/DSA sums/staircase.js
@@ -0,0 +1,98 @@
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//    let line = "";
+//    for (let j = 1; j<=i; j++)
+//    {
+//    line = line + "*";
+//    }
+//    space = "";
+//    space_count = n-i;
+//    for(let k = 1; k <= space_count; k++ ){
+//       space = space + " ";
+//    }
+//    console.log(space + line);
+// }  
+
+
+
+
+
+//Pramid Sum//
+
+// let n = 5;
+// let string = "";
+
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i; j++) {
+//     string += " ";
+//   }
+//   for (let k = 1; k <= i; k++) {
+//     string += "* ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+//diamond sum//
+
+// let n = 5;
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i; j++) {
+//     string += " ";
+//   }
+//   for (let k = 1; k <= i; k++) {
+//     string += "* ";
+//   }
+//   string += "\n";
+// }
+// for (let i = n - 1; i >= 1; i--) {
+//   for (let j = 1; j <= n - i; j++) {
+//     string += " ";
+//   }
+//   for (let k = 1; k <= i; k++) {
+//     string += "* ";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+
+
+//reverse string//
+
+
+// let str = "Pranav";
+// let strReverse = "";
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//     strReverse = strReverse + str[i];
+// }
+
+// console.log(strReverse);
+
+
+
+
+//String count//
+
+let str = "543786";
+let length = str.length;
+
+console.log("Length of the str: " + length);
+
+
+//Mutiplication of n
+
+
+let n = 23;
+let into = 10;
+let z = n * into;
+
+console.log("Value of Z is " + z);
+
+
+
+
